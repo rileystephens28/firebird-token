84be5adf367ddb423878d350b200a43e237c0030
Riley Stephens <riley.stephens28@gmail.com>
2023-03-30 16:33:21 -0500

    Add hardhat uniswap plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-ignore-warnings";
+import "@nomicfoundation/hardhat-toolbox";
+import "@onmychain/hardhat-uniswap-v2-deploy-plugin";
 
 const config: HardhatUserConfig = {
   solidity: {
